8063251acf659ffd5b7f26194e8c738e3dc3c036
vini-santos11 <m.vinicius1122o@gmail.com>
2021-02-26 19:59:54 -0300

    exercicio11 javascript

--- JavaScript/exercicio10/index.js
+++ JavaScript/exercicio10/index.js
@@ -1,11 +1,11 @@
-const hitchedSpaceships = [ ["Fenix", 8, true], 
-                          ["Golias", 10, true], 
-                          ["Helmet", 5, false], 
-                          ["Elemental", 3, true], 
-                          ["Darwin", 15, false] ]
+const hitchedSpaceships = [["Fenix", 8, true],
+                          ["Golias", 10, true],
+                          ["Helmet", 5, false],
+                          ["Elemental", 3, true],
+                          ["Darwin", 15, false]]
 
-let over9 = hitchedSpaceships.filter(element => { 
-   return element[1] > 9;
+let over9 = hitchedSpaceships.filter(element => {
+  return element[1] > 9;
 }).map(spaceship => {
   return spaceship[0];
 })
